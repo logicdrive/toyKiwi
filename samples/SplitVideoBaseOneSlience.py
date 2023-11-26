30f36e30dc63136a9dc60cf854408047b4c50280
logicdrive <logicdrive7@gmail.com>
2023-11-27 06:01:33 +0900

    Sample: 오디오를 자르기전에 오디오를 일정 간격으로 자르게 만듬

--- samples/SplitVideoBaseOneSlience.py
+++ samples/SplitVideoBaseOneSlience.py
@@ -9,11 +9,16 @@ if not os.path.exists("tmp/tmps"):
     os.makedirs("tmp/tmps")
 
 
-def splitVideosBaseOnSlience(targetFilePath:str, targetFileFormat:str, chunkDirPath:str, chunkFlieName:str, 
+def splitVideosBaseOnSlience(targetFilePath:str, targetFileFormat:str, chunkDirPath:str, chunkFlieName:str,
+                             cutStartMs:int=None, cutEndMs:int=None, 
                              minSilenceLen:int=250, silenceThresh:int=-50, keepSlience:bool=True) -> None:
     audioSegment = AudioSegment.from_file(targetFilePath, format=targetFileFormat)
+    if cutStartMs and cutEndMs : audioSegment = audioSegment[cutStartMs:cutEndMs]
+    elif cutStartMs : audioSegment = audioSegment[cutStartMs:]
+    elif cutEndMs : audioSegment = audioSegment[:cutEndMs]
+
     audioChunks = split_on_silence(audioSegment, min_silence_len=minSilenceLen, silence_thresh=silenceThresh, keep_silence=keepSlience)
     for i, chunk in enumerate(audioChunks):
         chunk.export(f"{chunkDirPath}/{chunkFlieName}_{i}.mp4", format=targetFileFormat)
 
-splitVideosBaseOnSlience("./tmp/temp.mp4", "mp4", "./tmp/tmps", "chunk")
+splitVideosBaseOnSlience("./tmp/temp.mp4", "mp4", "./tmp/tmps", "chunk", cutStartMs=1000*10, cutEndMs=1000*25)
