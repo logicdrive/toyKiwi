7cfdf552b8b8025c4bf6cab42b69bba853823aa1
logicdrive <logicdrive7@gmail.com>
2023-11-26 15:14:15 +0900

    Sample: 유튜브 URL로부터 파일을 받는 샘플 파일 추가

---
+++ samples/DownloadYoutubeVideo.py
@@ -0,0 +1,10 @@
+# 특정 유튜브 URL로부터 다운로드를 받는 샘플 파일
+
+import pytube
+
+def downloadYoutubeVideo(url:str, outputFilePath:str, outputFileName:str) -> None :
+    pytube.YouTube(url)\
+        .streams.get_highest_resolution()\
+        .download(output_path=outputFilePath, filename=outputFileName)
+
+downloadYoutubeVideo("https://www.youtube.com/watch?v=fkvhqBMmnCg", ".", "temp.mp4")
