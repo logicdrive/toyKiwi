a8515bb582b46a386ed64e34492d9615cd7e9a50
logicdrive <logicdrive7@gmail.com>
2023-12-01 15:58:07 +0900

    Sample: 파일을 AWS Public S3에 업로드하는 샘플 추가

---
+++ samples/UploadFileToAWSS3.py
@@ -0,0 +1,24 @@
+# 파일을 AWS Public S3 스토리지에 업로드해보는 샘플파일
+
+import boto3
+import os
+
+class AWSProxy :
+    def __init__(self) :
+        self.__CLIENT = boto3.Session(
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
+        
+    def uploadToPublicS3(self, filePath:str, bucketName:str, regionCode:str="ap-northeast-2") -> str :
+        objectKey = filePath.split("/")[-1]
+
+        s3 = self.__CLIENT.resource("s3")
+        s3.meta.client.upload_file(Filename=filePath, Bucket=bucketName, Key=objectKey)
+
+        obj = s3.Object(bucketName, objectKey)
+        obj.Acl().put(ACL="public-read")
+        return f"https://{bucketName}.s3.{regionCode}.amazonaws.com/{objectKey}"
+
+awsProxy = AWSProxy()
+print(awsProxy.uploadToPublicS3("./tempCutted.mp4", "sinsung6722-toywiki"))
