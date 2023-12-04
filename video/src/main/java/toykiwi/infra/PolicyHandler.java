eea3fb99a9e27d55eea7f33db8283c23125b16d7
logicdrive <logicdrive7@gmail.com>
2023-12-04 16:23:31 +0900

    Refactor: 불필요한 변수들 삭제

--- video/src/main/java/toykiwi/infra/PolicyHandler.java
+++ video/src/main/java/toykiwi/infra/PolicyHandler.java
@@ -2,13 +2,12 @@ package toykiwi.infra;
 
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import toykiwi.domain.Video;
 import toykiwi.config.kafka.KafkaProcessor;
-import toykiwi.domain.*;
 import toykiwi.event.UploadingVideoCompleted;
 import toykiwi.logger.CustomLogger;
 import toykiwi.logger.CustomLoggerType;
@@ -16,12 +15,8 @@ import toykiwi.logger.CustomLoggerType;
 @Service
 @Transactional
 public class PolicyHandler {
-    @Autowired
-    VideoRepository videoRepository;
-
     @StreamListener(KafkaProcessor.INPUT)
     public void whatever(@Payload String eventString) {}
-
 
     // 비디오 URL 업로드가 완료되었을 경우, 업로드된 URL을 반영시키기 위해서
     @StreamListener(
