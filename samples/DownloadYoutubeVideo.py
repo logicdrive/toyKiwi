7a8175024e32c45b021f238ab0f5a9d0b7924466
logicdrive <logicdrive7@gmail.com>
2023-12-05 14:13:06 +0900

    Sample: 유튜브 비디오와 관련된 추가적인 메타데이터를 추출하도록 만듬

--- samples/DownloadYoutubeVideo.py
+++ samples/DownloadYoutubeVideo.py
@@ -1,10 +1,24 @@
 # 특정 유튜브 URL로부터 다운로드를 받는 샘플 파일
 
 import pytube
+from dataclasses import dataclass
 
-def downloadYoutubeVideo(url:str, outputFilePath:str, outputFileName:str) -> None :
-    pytube.YouTube(url)\
-        .streams.get_highest_resolution()\
+
+@dataclass
+class VideoMetadataDto:
+    url: str
+    title: str
+    thumbnailUrl: str
+
+def downloadYoutubeVideo(url:str, outputFilePath:str, outputFileName:str) -> VideoMetadataDto :
+    youtube = pytube.YouTube(url)
+    youtube.streams.get_highest_resolution()\
         .download(output_path=outputFilePath, filename=outputFileName)
+    return VideoMetadataDto(url, youtube.title, youtube.thumbnail_url)
 
-downloadYoutubeVideo("https://www.youtube.com/watch?v=fkvhqBMmnCg", "./tmp", "temp.mp4")
+
+videoMetadataDto:VideoMetadataDto = downloadYoutubeVideo("https://www.youtube.com/watch?v=fkvhqBMmnCg", "./tmp", "temp.mp4")
+
+print(videoMetadataDto.url)
+print(videoMetadataDto.title)
+print(videoMetadataDto.thumbnailUrl)
