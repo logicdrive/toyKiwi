ee7c76e195da35153832198a46c7c501b5cf34d2
logicdrive <logicdrive7@gmail.com>
2023-12-08 22:48:21 +0900

    Feat: 커스텀 로그 관련 기반 마련

--- externalSystem/app/_global/CustomLogger.py
+++ externalSystem/app/_global/CustomLogger.py
@@ -1,4 +1,18 @@
 from flask import current_app
+from . import CustomLoggerType
+import traceback
 
-def debug(message) :
-    current_app.logger.debug(message)
+
+customLoggerTypeMap = {
+    CustomLoggerType.ENTER_EXIT: "ENTER/EXIT",
+    CustomLoggerType.ENTER: "ENTER",
+    CustomLoggerType.EXIT: "EXIT",
+    CustomLoggerType.EFFECT: "EFFECT"
+}
+
+
+def debug(type:CustomLoggerType, message:str="", params:str="{}") :
+    current_app.logger.debug("[{}] [{}] {}: {}".format(traceback.extract_stack()[-2], customLoggerTypeMap[type], message, params))
+
+def error(e:Exception, message:str="", params:str="{}") :
+    current_app.logger.error("[{}] [{}] {}: {}".format(traceback.extract_stack()[-2], e.__class__, message, params) + "\n" + "\n".join(map(str, traceback.extract_stack())))
--- externalSystem/app/sanityCheck/sanityCheckController.py
+++ externalSystem/app/sanityCheck/sanityCheckController.py
@@ -1,9 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, Response
 from . import SanityCheckService
+from .._global import CustomLogger
+from .._global import CustomLoggerType
 
 bp = Blueprint("sanityCheck", __name__, url_prefix="/sanityCheck")
 
 @bp.route("/")
 def sanityCheck() -> str:
-    SanityCheckService.sanityCheck()
-    return "sanityCheck"
+    CustomLogger.debug(CustomLoggerType.ENTER_EXIT)
+    CustomLogger.error(Exception("EXCEPTION"), "EXCEPTION TEST", "<test:{}>".format("TEST VALUE"))
+    return Response("", status=200)
