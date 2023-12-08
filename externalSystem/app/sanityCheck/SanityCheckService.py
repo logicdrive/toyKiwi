8b3b14b702bb678c039ec699116479796d7a9955
logicdrive <logicdrive7@gmail.com>
2023-12-09 02:02:06 +0900

    Feat: logs가 실제 파일에서 읽어오도록 만듬

--- externalSystem/app/sanityCheck/SanityCheckService.py
+++ externalSystem/app/sanityCheck/SanityCheckService.py
@@ -1,7 +1,21 @@
 from .LogsReqDto import LogsReqDto
 
 from .._global import CustomLogger
+from .._global import CustomLoggerType
 
+
+logFilePath:str = "./logs/logback.log"
 
 def logs(logsReqDto:LogsReqDto) -> list[str] :
-    return [str(logsReqDto.lineLength)]*logsReqDto.lineLength
+    readLogs:list[str] = []
+
+
+    CustomLogger.debug(CustomLoggerType.EFFECT, "", "<filePath: {}>".format(logFilePath))
+
+    with open(logFilePath, 'r') as f:
+        readLogs = f.readlines()[-logsReqDto.lineLength:]
+
+    CustomLogger.debug(CustomLoggerType.EFFECT, "", "<readLogsSize: {}>".format(len(readLogs)))
+
+
+    return readLogs
