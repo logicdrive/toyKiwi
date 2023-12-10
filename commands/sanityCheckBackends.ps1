cf977bc3a46d283c53a9d262db73eb27c6fa2f0f
logicdrive <logicdrive7@gmail.com>
2023-12-11 00:13:48 +0900

    Build: 일괄적인 Sanity Check를 위한 커맨드 추가

---
+++ commands/sanityCheckBackends.ps1
@@ -0,0 +1,5 @@
+http http://localhost:8082/sanityCheck # video
+http http://localhost:8083/sanityCheck # subtitle
+http http://localhost:8086/sanityCheck # externalSystemProxy
+http http://localhost:8084/sanityCheck # collectedData
+http http://localhost:8085/sanityCheck/ # externalSystem
