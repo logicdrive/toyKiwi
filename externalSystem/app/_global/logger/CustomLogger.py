203a871936b4629a2f9f71b4cd12e994cd68860c
logicdrive <logicdrive7@gmail.com>
2023-12-10 10:38:57 +0900

    Feat: 에러 로그가 더 구체적으로 출력되도록 개선

--- externalSystem/app/_global/logger/CustomLogger.py
+++ externalSystem/app/_global/logger/CustomLogger.py
@@ -16,4 +16,4 @@ def debug(type:CustomLoggerType, message:str="", params:str="{}") :
     current_app.logger.debug("[{}] [{}] {}: {}".format(traceback.extract_stack()[-2], customLoggerTypeMap[type], message, params))
 
 def error(e:Exception, message:str="", params:str="{}") :
-    current_app.logger.error("[{}] [{}] {}: {}".format(traceback.extract_stack()[-2], e.__class__, str(e) + " / " + message, params) + "\n" + "\n".join(map(str, traceback.extract_stack())))
+    current_app.logger.error("[{}] [{}] {}: {}".format(traceback.extract_stack()[-2], e.__class__, e.__doc__ + " / " + message, params) + "\n" + ''.join(traceback.format_tb(e.__traceback__)))
