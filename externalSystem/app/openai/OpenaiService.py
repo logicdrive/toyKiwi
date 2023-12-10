4f5c9ec8baed5be1a6900d99d44ebb4fb8cafb98
logicdrive <logicdrive7@gmail.com>
2023-12-10 19:59:31 +0900

    Feat: 주어진 URL에서 다운로드 받도록 만듬

--- externalSystem/app/openai/OpenaiService.py
+++ externalSystem/app/openai/OpenaiService.py
@@ -1,9 +1,19 @@
+import wget
+
+from .._global.workdir.WorkDirManager import WorkDirManager
+from .._global.logger import CustomLogger
+from .._global.logger import CustomLoggerType
+
 from .reqDtos.GenerateSubtitleReqDto import GenerateSubtitleReqDto
 from .resDtos.GenerateSubtitleResDto import GenerateSubtitleResDto
 from .resDtos.SubtitleResDto import SubtitleResDto
 
 # 주어진 동영상들을 분석해서 자막들을 추출해서 관련 정보들을 반환시키기 위해서
 def generateSubtitle(generateSubtitleReqDto:GenerateSubtitleReqDto) -> GenerateSubtitleResDto :
+    with WorkDirManager(isAfterClear=False) as path:
+        CustomLogger.debug(CustomLoggerType.EFFECT, "Try to download video", "<downloadUrl: {}>".format(generateSubtitleReqDto.uploadedUrl))
+        wget.download(generateSubtitleReqDto.uploadedUrl, path("video.mp4"))
+    
     subtitles:list = []
     subtitles.append(SubtitleResDto("Test Subtitle No.1", 0, 5))
     subtitles.append(SubtitleResDto("Test Subtitle No.2", 5, 10))
