1af75297a2123fad664be753fb88a0b533571f57
logicdrive <logicdrive7@gmail.com>
2023-12-11 13:11:41 +0900

    Feat: 학습 동영상 목록 관련 Nav 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VideoEditListPage from "./video/edit/list/VideoEditListPage";
 import VideoQuizTryPage from "./video/quiz/try/VideoQuizTryPage";
@@ -7,14 +8,16 @@ import VideoQuizResultPage from "./video/quiz/result/VideoQuizResultPage";
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-              <Route path="/" element={<VideoEditListPage/>} />
-              <Route path="/video/edit/list" element={<VideoEditListPage/>} />
-              <Route path="/video/quiz/try" element={<VideoQuizTryPage/>} />
-              <Route path="/video/quiz/result" element={<VideoQuizResultPage/>} />
-          </Routes>
-      </Router>
+      <Container maxWidth="sm">
+        <Router>
+          <Routes>
+                <Route path="/" element={<VideoEditListPage/>} />
+                <Route path="/video/edit/list" element={<VideoEditListPage/>} />
+                <Route path="/video/quiz/try" element={<VideoQuizTryPage/>} />
+                <Route path="/video/quiz/result" element={<VideoQuizResultPage/>} />
+            </Routes>
+        </Router>
+      </Container>
     </>
   )
 }
--- frontend/src/video/edit/list/VideoEditListPage.js
+++ frontend/src/video/edit/list/VideoEditListPage.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import { Link as RouterLink } from 'react-router-dom';
+import { Container, Toolbar, Link, Button, Typography } from '@mui/material';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 const VideoEditListPage = () => {
     return (
-        <>
-            <h1>
-                VideoEditListPage
-            </h1>
-        </>
+        <AppBar position="static" style={{backgroundColor:"crimson"}}>
+            <Container maxWidth="lg">
+                <Toolbar disableGutters>
+                    <Link component={RouterLink} to="/" variant="h5" underline="none" sx={{color: "white", fontWeight: "bolder", fontFamily: "BMDfont", flexGrow: 1}}>
+                        학습 동영상 목록
+                    </Link>
+
+                    <Link component={RouterLink} sx={{backgroundColor: "red", margin: 1, position: "relative", left: 4}}>
+                        <Button>
+                            <Typography sx={{color: "white", fontWeight: "bolder", fontFamily: "BMDfont", position: "relative", top: 3}}>
+                                <FileUploadIcon sx={{fontSize: 40}}/>
+                            </Typography>
+                        </Button>
+                    </Link>
+                </Toolbar>
+            </Container>
+
+            
+        </AppBar>
     );
 }
 
