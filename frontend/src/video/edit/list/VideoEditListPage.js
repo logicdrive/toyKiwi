c2371ba057a91c484088d214eb37624dd8077ab8
logicdrive <logicdrive7@gmail.com>
2023-12-12 21:43:47 +0900

    Refactor: 코드 영역 정돈 작업

--- frontend/src/video/edit/list/VideoEditListPage.js
+++ frontend/src/video/edit/list/VideoEditListPage.js
@@ -13,38 +13,14 @@ const VideoEditListPage = () => {
 
     const [uploadVideos, setUploadVideos] = useState([]);
     const [isUploadVideoMenuOpeneds, setIsUploadVideoMenuOpeneds] = useState([]);
-
-    const setUploadVideoMenuOpened = (index, value) => {
-        setIsUploadVideoMenuOpeneds((isUploadVideoMenuOpeneds) => {
-            let copiedValue = [...isUploadVideoMenuOpeneds]
-            copiedValue[index] = value
-            return copiedValue
-        })
-    }
-
-    const onDeleteUploadVideoButtonClicked = async (index) => {
-        setUploadVideoMenuOpened(index, false)
-
-        try {
-            
-            const videoIdToDelete = uploadVideos[index].videoId
-            console.log("[Effect] Try to delete video :", videoIdToDelete);
-            await axios.delete(`${APIConfig.videoUrl}/videos/${videoIdToDelete}`);
-
-            addAlertPopUp("비디오 삭제 요청이 정상적으로 완료되었습니다.", "success");
-
-        } catch (error) {
-            addAlertPopUp("비디오 삭제 요청 과정에서 오류가 발생했습니다!", "error");
-            console.error("비디오 삭제 요청 과정에서 오류가 발생했습니다!", error);
-        }
-    }
-
     useEffect(() => {
         (async () => {
             try {
 
+                console.log(`[EFFECT] Try to download video infos: <url:${`${APIConfig.collectedDataUrl}/videos`}>`)
                 const response = await axios.get(`${APIConfig.collectedDataUrl}/videos`);
                 const videos = response.data._embedded.videos;
+                console.log("[EFFECT] Downloaded videos:", response)
 
                 setIsUploadVideoMenuOpeneds(new Array(videos.length).fill(false))
                 setUploadVideos(response.data._embedded.videos.map((video) => {
@@ -65,8 +41,10 @@ const VideoEditListPage = () => {
         })()
     }, [addAlertPopUp])
 
+
+    // 입력된 정보를 기반으로 동영상 업로드 요청을 수행하기 위해서
     const onInputCompleted = async (userInputs) => {
-        console.log("[EFFECT] 동영상 업로드 요청이 수행됨:", userInputs);
+        console.log("[EFFECT] Try to upload video by using userInputs:", userInputs);
 
         const reqDto = {
             youtubeUrl: userInputs.youtubeUrl,
@@ -75,7 +53,33 @@ const VideoEditListPage = () => {
         }
         await axios.post(`${APIConfig.videoUrl}/videos`, reqDto);
     }
-    
+
+    // 삭제 버튼을 누를시에 비디오 삭제 요청을 수행하기 위해서
+    const onDeleteUploadVideoButtonClicked = async (index) => {
+        setUploadVideoMenuOpened(index, false)
+
+        try {
+            
+            const videoIdToDelete = uploadVideos[index].videoId
+            console.log("[Effect] Try to delete video :", videoIdToDelete);
+            await axios.delete(`${APIConfig.videoUrl}/videos/${videoIdToDelete}`);
+
+            addAlertPopUp("비디오 삭제 요청이 정상적으로 완료되었습니다.", "success");
+
+        } catch (error) {
+            addAlertPopUp("비디오 삭제 요청 과정에서 오류가 발생했습니다!", "error");
+            console.error("비디오 삭제 요청 과정에서 오류가 발생했습니다!", error);
+        }
+    }
+
+
+    const setUploadVideoMenuOpened = (index, value) => {
+        setIsUploadVideoMenuOpeneds((isUploadVideoMenuOpeneds) => {
+            let copiedValue = [...isUploadVideoMenuOpeneds]
+            copiedValue[index] = value
+            return copiedValue
+        })
+    }
 
     return (
         <>
