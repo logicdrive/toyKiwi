97b648f60f5c68eb77e86070fd070fca53e9f05a
logicdrive <logicdrive7@gmail.com>
2023-12-12 09:52:25 +0900

    Feat: 퀴즈시에 필요한 데이터들을 조회하도록 만듬

--- frontend/src/video/quiz/try/VideoQuizTryPage.js
+++ frontend/src/video/quiz/try/VideoQuizTryPage.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
 import { Container, Toolbar, Link, Button, Typography, TextField,
     Dialog, DialogTitle, DialogContent, DialogActions, Card, CardContent, Grid, CardMedia, IconButton, Menu, MenuItem, AppBar } from '@mui/material';
+import { AlertPopupContext } from '../../../_global/alertPopUp/AlertPopUpContext'
+import APIConfig from '../../../APIConfig';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const VideoQuizTryPage = () => {
+    const { addAlertPopUp } = useContext(AlertPopupContext);
     const [queryParameters] = useSearchParams()
     const navigate = useNavigate();
+
+    const [uploadVideoInfo, setUploadVideoInfo] = useState({});
+    const [subtitleInfos, setSubtitleInfos] = useState([])
+    useEffect(() => {
+        (async () => {
+            try {
+
+                const response = await axios.get(`${APIConfig.collectedDataUrl}/videos/${queryParameters.get("videoId")}`);
+                setUploadVideoInfo({
+                    videoId: response.data.videoId,
+                    uploadedUrl: response.data.uploadedUrl
+                })
+
+            } catch (error) {
+                addAlertPopUp("업로된 동영상 정보를 가져오는 과정에서 오류가 발생했습니다!", "error");
+                console.error("업로된 동영상 정보를 가져오는 과정에서 오류가 발생했습니다!", error);
+            }
+
+            try {
+
+                const response = await axios.get(`${APIConfig.collectedDataUrl}/videos/${queryParameters.get("videoId")}/subtitles`);
+                setSubtitleInfos(response.data.subtitles.map((subtitle) => {
+                    return {
+                        subtitleId: subtitle.subtitleId,
+                        subtitle: subtitle.subtitle,
+                        translatedSubtitle: subtitle.translatedSubtitle,
+                        startSecond: subtitle.startSecond,
+                        endSecond: subtitle.endSecond
+                    }
+                }))
+
+            } catch (error) {
+                addAlertPopUp("업로된 동영상 자막 정보를 가져오는 과정에서 오류가 발생했습니다!", "error");
+                console.error("업로된 동영상 자막 정보를 가져오는 과정에서 오류가 발생했습니다!", error);
+            }
+        })()
+    }, [addAlertPopUp])
 
     return (
         <>
