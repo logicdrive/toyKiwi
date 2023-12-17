56d6fb8c5a784cb1422e30e28184c46fd20c6250
logicdrive <logicdrive7@gmail.com>
2023-12-17 19:59:15 +0900

    Build: 도커 이미지 빌드 기능 구현

---
+++ video/commands/docker/remove.ps1
@@ -0,0 +1,3 @@
+docker container stop toykiwi_video_con
+docker container rm toykiwi_video_con
+docker image rm sinsung6722/toykiwi_video:v1
---
+++ video/commands/docker/shell.ps1
@@ -0,0 +1,1 @@
+docker run --rm -it --entrypoint /bin/sh --net host --name toykiwi_video_con sinsung6722/toykiwi_video:v1
