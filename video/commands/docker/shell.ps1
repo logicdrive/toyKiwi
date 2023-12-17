95adbe508bbcc1185480757d36e466cd3cac7dae
logicdrive <logicdrive7@gmail.com>
2023-12-17 20:25:28 +0900

    Fix: 도커상에서 8080포트가 열리도록 개선

--- video/commands/docker/shell.ps1
+++ video/commands/docker/shell.ps1
@@ -1,1 +1,1 @@
-docker run --name toykiwi_video_con -e SPRING_PROFILES_ACTIVE=docker -p 8082:8082 --rm -it --entrypoint /bin/sh sinsung6722/toykiwi_video:v1
+docker run --name toykiwi_video_con -e SPRING_PROFILES_ACTIVE=docker -p 8082:8080 --rm -it --entrypoint /bin/sh sinsung6722/toykiwi_video:v1
