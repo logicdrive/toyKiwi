6bc19d97924483057b782c17b93834f7e1b146f7
logicdrive <logicdrive7@gmail.com>
2023-12-18 11:45:21 +0900

    Build: Flask Docker 이미지를 위한 기반 마련

--- externalSystem/commands/run.ps1
+++ externalSystem/commands/run.ps1
@@ -1,1 +1,17 @@
-bash.exe ./commands/run.sh
+.\venv\Scripts\activate
+
+
+
+cp envs/development.env .flaskenv
+
+
+if (Test-Path "logs") {
+    Remove-Item -Recurse -Force "logs"
+}
+
+if (Test-Path "workDirs") {
+    Remove-Item -Recurse -Force "workDirs"
+}
+
+
+python -m flask run
