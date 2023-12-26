df6be5d49620ecb25599f67219d4994fb079e881
logicdrive <logicdrive7@gmail.com>
2023-12-26 15:12:26 +0900

    Refactor: 불필요한 코드 간소화 작업

--- collectedData/src/main/java/toykiwi/webSocket/sanityCheck/SanityCheckSocketHandler.java
+++ collectedData/src/main/java/toykiwi/webSocket/sanityCheck/SanityCheckSocketHandler.java
@@ -1,10 +1,8 @@
 package toykiwi.webSocket.sanityCheck;
 
 import java.io.IOException;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -16,37 +14,21 @@ import toykiwi._global.logger.CustomLoggerType;
 @Component
 public class SanityCheckSocketHandler extends TextWebSocketHandler {
 
-    private static final ConcurrentHashMap<String, WebSocketSession> CLIENTS = new ConcurrentHashMap<String, WebSocketSession>();
-
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        CLIENTS.put(session.getId(), session);
-    }
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        CLIENTS.remove(session.getId());
-    }
-
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String id = session.getId();
-        CLIENTS.entrySet().forEach( arg->{
-            if(arg.getKey().equals(id)) {
-                try {
-                    
-                    SanityCheckReqDto sanityCheckReqDto = new SanityCheckReqDto(message);
-                    CustomLogger.debug(CustomLoggerType.ENTER, "", String.format("{sanityCheckReqDto: %s}", sanityCheckReqDto.toString()));
+        try {
+            
+            SanityCheckReqDto sanityCheckReqDto = new SanityCheckReqDto(message);
+            CustomLogger.debug(CustomLoggerType.ENTER, "", String.format("{sanityCheckReqDto: %s}", sanityCheckReqDto.toString()));
 
-                    SanityCheckResDto sanityCheckResDto = new SanityCheckResDto(sanityCheckReqDto.getMessage());
-                    CustomLogger.debug(CustomLoggerType.EXIT, "", String.format("{sanityCheckResDto: %s}", sanityCheckResDto.toString()));
-                    
-                    arg.getValue().sendMessage(sanityCheckResDto.jsonTextMessage());
+            SanityCheckResDto sanityCheckResDto = new SanityCheckResDto(sanityCheckReqDto.getMessage());
+            CustomLogger.debug(CustomLoggerType.EXIT, "", String.format("{sanityCheckResDto: %s}", sanityCheckResDto.toString()));
+            
+            session.sendMessage(sanityCheckResDto.jsonTextMessage());
 
-                } catch (IOException e) {
-                    CustomLogger.error(e, "", String.format("{message: %s}", message.toString()));
-                }
-            }
-        });
+        } catch (IOException e) {
+            CustomLogger.error(e, "", String.format("{message: %s}", message.toString()));
+        }
     }
+    
 }
